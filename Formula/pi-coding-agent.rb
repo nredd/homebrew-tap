02dcdf5393250d66506af056088e809b547f5467
Nicholas Redd <nicholaseredd@gmail.com>
2026-09-21 16:25:41 -0700

    pi-coding-agent: bump to v0.85.1-nredd.6
    Collapse all built-in tool call previews (edit, write, bash, grep,
    find, ls) behind expand instead of always rendering a body preview
    inline. See nredd/pi@187bb4114.

--- Formula/pi-coding-agent.rb
+++ Formula/pi-coding-agent.rb
@@ -1,9 +1,9 @@
 class PiCodingAgent < Formula
   desc "AI agent toolkit with nredd transcript disclosures"
   homepage "https://github.com/nredd/pi"
-  url "https://github.com/nredd/pi/releases/download/v0.85.1-nredd.5/earendil-works-pi-coding-agent-0.85.1.tgz"
-  version "0.85.1-nredd.5"
-  sha256 "f5d6529497bc3bbf05eff9ce6471c9200ddd8d369211212d3b080487b85d989e"
+  url "https://github.com/nredd/pi/releases/download/v0.85.1-nredd.6/earendil-works-pi-coding-agent-0.85.1.tgz"
+  version "0.85.1-nredd.6"
+  sha256 "69c24ef0f3d20c96b6c4a64edb1c2528de3e295f13a2cdbdd7e36551b70a4765"
   license "MIT"
 
   depends_on "node"
@@ -18,7 +18,7 @@ class PiCodingAgent < Formula
   end
 
   resource "pi-tui" do
-    url "https://github.com/nredd/pi/releases/download/v0.85.1-nredd.5/earendil-works-pi-tui-0.85.1.tgz"
+    url "https://github.com/nredd/pi/releases/download/v0.85.1-nredd.6/earendil-works-pi-tui-0.85.1.tgz"
     sha256 "7c46f8fb0290d261095289e3adbe5a9cfc6da5ba0183df56aee62173671a235d"
   end
 
